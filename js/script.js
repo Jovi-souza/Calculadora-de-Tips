f94f4c1e5615a781d9b35a8a85da7ba12b323517
João Vitor Rodrigues de souza <jv479439@gmail.com>
2022-06-10 00:19:24 -0300

    finalizando projeto

--- js/script.js
+++ js/script.js
@@ -1,87 +1,19 @@
-// function calcularPorcentagem5() {
-
-//     let accountValue = Number(document.querySelector("#accountValue").value)
-//     let numberOfPeople = Number(document.querySelector("#numberOfPeople").value)
-
-//     let percentage = 5
-
-//     let calcular = accountValue / numberOfPeople
-//     let calcPerce = (percentage / 100) * calcular
-
-//     pricePerson.innerHTML = `$${calcPerce.toFixed(2)}`
-
-//     let somaDaConta = calcPerce + calcular
-
-//     priceTotal.innerHTML = `$${somaDaConta.toFixed(2)}`
-// }
-
-// function calcularPorcentagem10() {
-//     let accountValue = Number(document.querySelector("#accountValue").value)
-//     let numberOfPeople = Number(document.querySelector("#numberOfPeople").value)
-//     let percentage = 10
-
-//     let calcular = accountValue / numberOfPeople
-//     let calcPerce = (percentage / 100) * calcular
-
-//     pricePerson.innerHTML = `$${calcPerce.toFixed(2)}`
-
-//     let somaDaConta = calcPerce + calcular
-
-//     priceTotal.innerHTML = `$${somaDaConta.toFixed(2)}`
-// }
-
-// function calcularPorcentagem15() {
-//     let accountValue = Number(document.querySelector("#accountValue").value)
-//     let numberOfPeople = Number(document.querySelector("#numberOfPeople").value)
-//     let percentage = 15
-
-//     let calcular = accountValue / numberOfPeople
-//     let calcPerce = (percentage / 100) * calcular
-
-//     pricePerson.innerHTML = `$${calcPerce.toFixed(2)}`
-
-//     let somaDaConta = calcPerce + calcular
-
-//     priceTotal.innerHTML = `$${somaDaConta.toFixed(2)}`
-// }
-
-// function calcularPorcentagem25() {
-//     let accountValue = Number(document.querySelector("#accountValue").value)
-//     let numberOfPeople = Number(document.querySelector("#numberOfPeople").value)
-//     let percentage = 25
-
-//     let calcular = accountValue / numberOfPeople
-//     let calcPerce = (percentage / 100) * calcular
-
-//     pricePerson.innerHTML = `$${calcPerce.toFixed(2)}`
-//     let somaDaConta = calcPerce + calcular
-
-//     priceTotal.innerHTML = `$${somaDaConta.toFixed(2)}`
-// }
-
-// function calcularPorcentagem50() {
-//     let accountValue = Number(document.querySelector("#accountValue").value)
-//     let numberOfPeople = Number(document.querySelector("#numberOfPeople").value)
-
-//     let percentage = Number(document.querySelector('#percentage').value)
-
-//     let calcular = accountValue / numberOfPeople
-//     let calcPerce = (percentage / 100) * calcular
-
-//     pricePerson.innerHTML = `$${calcPerce.toFixed(2)}`
-
-//     let somaDaConta = calcPerce + calcular
-
-//     priceTotal.innerHTML = `$${somaDaConta.toFixed(2)}`
-// }
+function reset() {
+    let reset = '$0.00'
+    pricePerson.innerHTML = `${reset}`
+    priceTotal.innerHTML = `${reset}`
+    accountAmount.value = ''
+    numberOfPeople.value = ''
+    Custom.value = ''
+}
 
 function calculateTipPercentageCustom() {
-    let accountValue = Number(document.querySelector("#accountValue").value)
-    let numberOfPeople = Number(document.querySelector("#numberOfPeople").value)
+    const accountAmount = Number(document.querySelector("#accountAmount").value)
+    const numberOfPeople = Number(document.querySelector("#numberOfPeople").value)
 
     let percentage = Number(document.querySelector('#Custom').value)
 
-    let divideValuePerPerson = accountValue / numberOfPeople
+    let divideValuePerPerson = accountAmount / numberOfPeople
     let applyPercentage = (percentage / 100) * divideValuePerPerson
 
     pricePerson.innerHTML = `$${applyPercentage.toFixed(2)}`
@@ -91,15 +23,25 @@ function calculateTipPercentageCustom() {
     priceTotal.innerHTML = `$${totalAcount.toFixed(2)}`
 }
 
-//     função final
-function calculateTipPercentage() {
-    let accountValue = Number(document.querySelector("#accountValue").value)
-    let numberOfPeople = Number(document.querySelector("#numberOfPeople").value)
+function percentage5() {
+    const accountAmount = Number(document.querySelector("#accountAmount").value)
+    const numberOfPeople = Number(document.querySelector("#numberOfPeople").value)
 
-    let percentage = Number(document.querySelector('#percentage').value)
+    let divideValuePerPerson = accountAmount / numberOfPeople
+    let applyPercentage = (5 / 100) * divideValuePerPerson
 
-    let divideValuePerPerson = accountValue / numberOfPeople
-    let applyPercentage = (percentage / 100) * divideValuePerPerson
+    let totalAcount = applyPercentage + divideValuePerPerson
+
+    pricePerson.innerHTML = `$${applyPercentage.toFixed(2)}`
+    priceTotal.innerHTML = `$${totalAcount.toFixed(2)}`
+}
+
+function percentage10() {
+    const accountAmount = Number(document.querySelector("#accountAmount").value)
+    const numberOfPeople = Number(document.querySelector("#numberOfPeople").value)
+
+    let divideValuePerPerson = accountAmount / numberOfPeople
+    let applyPercentage = (10 / 100) * divideValuePerPerson
 
     pricePerson.innerHTML = `$${applyPercentage.toFixed(2)}`
 
@@ -108,9 +50,44 @@ function calculateTipPercentage() {
     priceTotal.innerHTML = `$${totalAcount.toFixed(2)}`
 }
 
-function reset() {
-    let clean = '$0.00'
-    pricePerson.innerHTML = `${clean}`
-    priceTotal.innerHTML = `${clean}`
+function percentage15() {
+    const accountAmount = Number(document.querySelector("#accountAmount").value)
+    const numberOfPeople = Number(document.querySelector("#numberOfPeople").value)
+
+    let divideValuePerPerson = accountAmount / numberOfPeople
+    let applyPercentage = (15 / 100) * divideValuePerPerson
+
+    pricePerson.innerHTML = `$${applyPercentage.toFixed(2)}`
+
+    let totalAcount = applyPercentage + divideValuePerPerson
+
+    priceTotal.innerHTML = `$${totalAcount.toFixed(2)}`
 }
 
+function percentage25() {
+    const accountAmount = Number(document.querySelector("#accountAmount").value)
+    const numberOfPeople = Number(document.querySelector("#numberOfPeople").value)
+
+    let divideValuePerPerson = accountAmount / numberOfPeople
+    let applyPercentage = (25 / 100) * divideValuePerPerson
+
+    pricePerson.innerHTML = `$${applyPercentage.toFixed(2)}`
+
+    let totalAcount = applyPercentage + divideValuePerPerson
+
+    priceTotal.innerHTML = `$${totalAcount.toFixed(2)}`
+}
+
+function percentage50() {
+    const accountAmount = Number(document.querySelector("#accountAmount").value)
+    const numberOfPeople = Number(document.querySelector("#numberOfPeople").value)
+
+    let divideValuePerPerson = accountAmount / numberOfPeople
+    let applyPercentage = (50 / 100) * divideValuePerPerson
+
+    pricePerson.innerHTML = `$${applyPercentage.toFixed(2)}`
+
+    let totalAcount = applyPercentage + divideValuePerPerson
+
+    priceTotal.innerHTML = `$${totalAcount.toFixed(2)}`
+}
